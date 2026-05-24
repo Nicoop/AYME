c2b17fa32f176b20762ffa6db8c7dfda5e7e6cf7
JuliAvila <julieta.avila@uncuyo.edu.ar>
2026-05-23 23:46:10 -0300

    Simulaciondinamica

--- Simulacion_Dinamica.m
+++ Simulacion_Dinamica.m
@@ -1,34 +1,102 @@
 %% 2. EJECUTAR LA SIMULACIÓN DESDE EL SCRIPT
-% Reemplaza 'tu_modelo' por el nombre exacto de tu archivo .slx
 simOut = sim('Simulacion_Temporal.slx', 'SimulationMode', 'normal');
 
 %% 3. EXTRACCIÓN DE LOS DATOS EXPORTADOS
-% Extraemos el contenedor de datos y luego cada señal por su nombre en Simulink
 logs = simOut.logsout;
 
-% Suponiendo que tus cables en Simulink se llaman 'i_qs' y 'w_m'
-sig_iqs = logs.get('i_qs').Values;
-sig_wm  = logs.get('w_m').Values;
+iqs_LTI    = logs.get('i_qs_LTI').Values;
+wm_LTI     = logs.get('w_m_LTI').Values;
+thetam_LTI = logs.get('theta_m_LTI').Values;
+Ts_LTI     = logs.get('T_s_LTI').Values;
 
-% MATLAB los guarda como objetos 'Timeseries'. Extraemos tiempo y datos:
-t_sim  = sig_iqs.Time;       % Vector de tiempo común
-iqs_data = sig_iqs.Data;     % Vector de datos de corriente
-wm_data  = sig_wm.Data;      % Vector de datos de velocidad
+iqs_NL    = logs.get('i_qs_NL').Values;
+wm_NL     = logs.get('w_m_NL').Values;
+thetam_NL = logs.get('theta_m_NL').Values;
+Ts_NL     = logs.get('T_s_NL').Values;
 
-%% 4. Gráficos Profesionales para el Informe
-figure('Units', 'inches', 'Position', [1, 1, 6, 4]); % Tamaño fijo para que no se deforme en el PDF
+% Extracción de vectores de tiempo y datos 
+t_sim           = iqs_LTI.Time;       
+iqs_LTI_data    = iqs_LTI.Data;     
+wm_LTI_data     = wm_LTI.Data;      
+thetam_LTI_data = thetam_LTI.Data; 
+Ts_LTI_data     = Ts_LTI.Data; 
 
-subplot(2,1,1);
-plot(t_sim, iqs_data, 'LineWidth', 1.5, 'Color', [0 0.4470 0.7410]);
+iqs_NL_data    = iqs_NL.Data;     
+wm_NL_data     = wm_NL.Data;      
+thetam_NL_data = thetam_NL.Data; 
+Ts_NL_data     = Ts_NL.Data; 
+
+%% 4. Gráficos LTI
+figure('Units', 'inches', 'Position', [1, 1, 6, 7]); 
+
+% Subplot 1: Corriente
+subplot(4,1,1);
+plot(t_sim, iqs_LTI_data, 'LineWidth', 1.5, 'Color', [0 0.4470 0.7410]);
 grid on;
-ylabel('$i_{qs}^r$ [A]', 'Interpreter', 'latex', 'FontSize', 11);
-title('Respuesta Dinámica de la Ley de Control', 'Interpreter', 'latex', 'FontSize', 12);
+ylabel('Corriente [A]', 'Interpreter', 'latex', 'FontSize', 10);
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$i_qs(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
 
-subplot(2,1,2);
-plot(t_sim, wm_data, 'LineWidth', 1.5, 'Color', [0.8500 0.3250 0.0980]);
+% Subplot 2: Velocidad
+subplot(4,1,2);
+plot(t_sim, wm_LTI_data, 'LineWidth', 1.5, 'Color', [0.8500 0.3250 0.0980]);
 grid on;
-xlabel('Tiempo [s]', 'Interpreter', 'latex', 'FontSize', 11);
-ylabel('$\omega_m$ [rad/s]', 'Interpreter', 'latex', 'FontSize', 11);
+ylabel('Velocidad [rad/s]','Interpreter', 'latex', 'FontSize', 10);
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$\omega_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
 
-% Guardar la imagen directamente en alta definición
-print('resultado_control_nolineal', '-dpng', '-r300');
+% Subplot 3: Posición Angular (Corregido nombre de variable)
+subplot(4,1,3);
+plot(t_sim, thetam_LTI_data, 'LineWidth', 1.5, 'Color', [0.4660 0.6740 0.1880]);
+grid on;
+ylabel('Posicion [rad]','Interpreter', 'latex', 'FontSize', 10); 
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$\theta_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+% Subplot 4: Torque
+subplot(4,1,4);
+plot(t_sim, Ts_LTI_data, 'LineWidth', 1.5, 'Color', [0.6350 0.0780 0.1840]);
+grid on;
+xlabel('Tiempo [s]','Interpreter', 'latex', 'FontSize', 10);
+ylabel('Torque [N$\cdot$m]', 'Interpreter', 'latex', 'FontSize', 10);
+title('$T_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+print('resultado_control_LTI', '-dpng', '-r300');
+
+
+%% Graficos NL
+figure('Units', 'inches', 'Position', [1, 1, 6, 7]); 
+
+% Subplot 1: Corriente
+subplot(4,1,1);
+plot(t_sim, iqs_NL_data, 'LineWidth', 1.5, 'Color', [0 0.4470 0.7410]);
+grid on;
+ylabel('Corriente [A]', 'Interpreter', 'latex', 'FontSize', 10);
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$i_qs(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+% Subplot 2: Velocidad
+subplot(4,1,2);
+plot(t_sim, wm_NL_data, 'LineWidth', 1.5, 'Color', [0.8500 0.3250 0.0980]);
+grid on;
+ylabel('Velocidad [rad/s]','Interpreter', 'latex', 'FontSize', 10);
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$\omega_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+% Subplot 3: Posición Angular (Corregido nombre de variable)
+subplot(4,1,3);
+plot(t_sim, thetam_NL_data, 'LineWidth', 1.5, 'Color', [0.4660 0.6740 0.1880]);
+grid on;
+ylabel('Posicion [rad]','Interpreter', 'latex', 'FontSize', 10); 
+xlabel('Tiempo[t]','Interpreter', 'latex', 'FontSize', 10);
+title('$\theta_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+% Subplot 4: Torque
+subplot(4,1,4);
+plot(t_sim, Ts_NL_data, 'LineWidth', 1.5, 'Color', [0.6350 0.0780 0.1840]);
+grid on;
+xlabel('Tiempo [s]','Interpreter', 'latex', 'FontSize', 10);
+ylabel('Torque [N$\cdot$m]', 'Interpreter', 'latex', 'FontSize', 10);
+title('$T_m(t)$', 'Interpreter', 'latex', 'FontSize', 14, 'FontWeight','bold');
+
+print('resultado_control_NL', '-dpng', '-r300');
